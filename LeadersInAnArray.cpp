f34223b167b268361d0424c16c41da418a4df5d9
Md-Sohail-Ahmed <sohailmj333@gmail.com>
2026-06-29 13:05:03 +0530

    Add problem statement for leader in array function
    Added a problem statement comment for clarity on the leader-finding algorithm.

--- LeadersInAnArray.cpp
+++ LeadersInAnArray.cpp
@@ -1,3 +1,11 @@
+/*Problem statement
+Given a sequence of numbers. Find all leaders in sequence. An element is a leader if it is strictly greater than all the elements on its right side.
+
+Note:
+1. Rightmost element is always a leader.
+
+2. The order of elements in the return sequence must be the same as the given sequence**/
+
 #include <bits/stdc++.h> 
 #include<vector>
 vector<int> findLeaders(vector<int> &nums, int n) {
